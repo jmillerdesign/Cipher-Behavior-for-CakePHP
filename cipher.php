c79763fe6b7edb16ba0b937160eb11eac1693d76
J Miller <j@jmillerdesign.com>
2011-12-01 22:31:43 -0800

    Allow to use Mcrypt instead of Security::cipher to encrypt/decrypt.

--- cipher.php
+++ cipher.php
@@ -14,11 +14,13 @@ class CipherBehavior extends ModelBehavior {
  *      - fields array Fields to cipher.								DEFAULT: password
  *      - autoDecrypt boolean Decrypt ciphered value automatically.		DEFAULT: true
  *      - key string Key to encrypt with.								DEFAULT: Security.salt
+ *      - cipher string Cipher method to use. (cake|mcrypt|auto)		DEFAULT: auto
  */
 	var $default = array(
 		'fields' => array('password'),
 		'autoDecrypt' => true,
-		'key' => ''
+		'key' => '',
+		'cipher' => 'auto'
 	);
 
 /**
@@ -42,16 +44,27 @@ class CipherBehavior extends ModelBehavior {
 
 		$this->config[$model->name] = $this->default;
 
+		// Cipher method
+		if (isset($config['cipher'])) {
+			$this->config[$model->name]['cipher'] = $config['cipher'];
+		}
+		$this->config[$model->name]['cipher'] = $this->_cipherMethod();
+
+		// Key
 		if (isset($config['key'])) {
-			$this->config[$model->name]['key'] = (string) $config['key'];
+			$this->config[$model->name]['key'] = $config['key'];
+		} else if ($this->config[$model->name]['cipher'] == 'mcrypt') {
+			$this->config[$model->name]['key'] = substr(Configure::read('Security.salt'), 0, 24);
 		} else {
 			$this->config[$model->name]['key'] = Configure::read('Security.salt');
 		}
 
+		// Fields
 		if (isset($config['fields'])) {
 			$this->config[$model->name]['fields'] = (array) $config['fields'];
 		}
 
+		// Auto-Decrypt
 		if (isset($config['autoDecrypt'])) {
 			$this->config[$model->name]['autoDecrypt'] = (bool) $config['autoDecrypt'];
 		}
@@ -121,7 +134,25 @@ class CipherBehavior extends ModelBehavior {
  * @return string Encrypted value
  */
 	public function encrypt($value, $settings) {
-		return Security::cipher($value, $settings['key']);
+		if ($settings['cipher'] == 'cake') {
+			return Security::cipher($value, $settings['key']);
+		}
+
+		$iv = 'fYfhHeDm';	// 8 bit IV
+		$bitCheck = 8;		// bit amount for diff algor.
+
+		$textNum = str_split($value, $bitCheck);
+		$textNum = $bitCheck - strlen($textNum[count($textNum) - 1]);
+
+		for ($i = 0; $i < $textNum; $i++) {
+			$value = $value . chr($textNum);
+		}
+
+		$cipher = mcrypt_module_open(MCRYPT_TRIPLEDES, '', 'cbc', '');
+		mcrypt_generic_init($cipher, $settings['key'], $iv);
+		$decrypted = mcrypt_generic($cipher, $value);
+		mcrypt_generic_deinit($cipher);
+		return base64_encode($decrypted);
 	}
 
 /**
@@ -132,17 +163,25 @@ class CipherBehavior extends ModelBehavior {
  * @return string Decrypted value
  */
 	public function decrypt($value, $settings) {
-		return Security::cipher($value, $settings['key']);
-	}
+		if ($settings['cipher'] == 'cake') {
+			return Security::cipher($value, $settings['key']);
+		}
 
-/**
- * Check if array is associative (keys are not numeric, incrementing sequentially from zero).
- *
- * @param array $array The array to check
- * @return boolean True if array is associative
- */
-	private function isAssoc($array) {
-		return (is_array($array) && (count($array) == 0 || 0 !== count(array_diff_key($array, array_keys(array_keys($array))))));
+		$iv = 'fYfhHeDm';	// 8 bit IV
+		$bitCheck = 8;		// bit amount for diff algor.
+
+		$cipher = mcrypt_module_open(MCRYPT_TRIPLEDES, '', 'cbc', '');
+		mcrypt_generic_init($cipher, $settings['key'], $iv);
+		$decrypted = @mdecrypt_generic($cipher, base64_decode($value));
+		mcrypt_generic_deinit($cipher);
+		$lastChar = substr($decrypted, -1);
+		for ($i = 0; $i < ($bitCheck - 1); $i++) {
+		    if (chr($i) == $lastChar) {
+		        $decrypted = substr($decrypted, 0, strlen($decrypted) - $i);
+		        break;
+		    }
+		}
+		return $decrypted;
 	}
 
 /**
@@ -153,5 +192,24 @@ class CipherBehavior extends ModelBehavior {
 	private function _cipherSeedValidates() {
 		$seed = Configure::read('Security.cipherSeed');
 		return ($seed && is_numeric($seed));
+	}
+
+/**
+ * Get chosen cipher method
+ *
+ * @return string (mcrypt|cake) Chosen cipher method
+ */
+	private function _cipherMethod() {
+		if ($this->config[$model->name]['cipher'] == 'auto') {
+			if (function_exists('mcrypt_module_open')) {
+				return 'mcrypt';
+			}
+		}
+
+		if ($this->config[$model->name]['cipher'] == 'mcrypt') {
+			return 'mcrypt';
+		}
+
+		return 'cake';
 	}
 }
